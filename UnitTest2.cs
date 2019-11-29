8ad997126befbb54ef4f24a2aaeb9597afdbb675
yadavbrij <58269565+yadavbrij@users.noreply.github.com>
2019-11-29 12:56:41 +0530

    Add files via upload

---
+++ UnitTest2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject2
+{
+    /// <summary>
+    /// Summary description for UnitTest2
+    /// </summary>
+    [TestClass]
+    public class UnitTest2
+    {
+        public void verfiy_UnitTest2()
+        {
+            Console.WriteLine("sayan");
+
+
+        }
+
+      
+    
+        [TestMethod]
+        public void TestMethod1()
+        {
+        UnitTest2 ut = new UnitTest2();
+        ut.verfiy_UnitTest2();
+        }
+    }
+}
